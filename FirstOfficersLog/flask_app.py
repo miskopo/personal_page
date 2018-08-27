f83715031df5f8834e6388aa1c0d7d5749b839cf
Michal Polovka <michal.polovka@gmail.com>
2018-08-26 21:59:47 -0600

    activate debug mode

--- FirstOfficersLog/flask_app.py
+++ FirstOfficersLog/flask_app.py
@@ -1,7 +1,10 @@
 from views import blog_view
+from logging import getLogger
 from FirstOfficersLog import app
 
 app.register_blueprint(blog_view)
 
+logger = getLogger(__name__)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
