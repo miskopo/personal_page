6b1518bd6101276ada22c63ae038ccc49df6111c
Michal Polovka <michal.polovka@gmail.com>
2018-08-26 22:05:14 -0600

    Temporarily disabled views.py

--- FirstOfficersLog/flask_app.py
+++ FirstOfficersLog/flask_app.py
@@ -2,9 +2,14 @@ from views import blog_view
 from logging import getLogger
 from FirstOfficersLog import app
 
-app.register_blueprint(blog_view)
+# app.register_blueprint(blog_view)
 
-logger = getLogger(__name__)
+# logger = getLogger(__name__)
+
+@app.route('/')
+def home():
+    return "I work"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
